3174639e2b8ea6348c7f10374c0fad3f0390289c
Warren <deed821@gmail.com>
2014-03-10 19:32:22 +1100

    lowered maxlayer for compatibility for camera apps
    Change-Id: Ie2a54672a866bc4c2e9f09750c4f03c13dbaf007

--- fuji.mk
+++ fuji.mk
@@ -264,7 +264,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     persist.hwc.mdpcomp.enable=true \
     debug.composition.type=dyn \
-    debug.mdpcomp.maxlayer=3 \
+    debug.mdpcomp.maxlayer=2 \
     debug.mdpcomp.logs=0 \
     debug.egl.recordable.rgba8888=1
 
